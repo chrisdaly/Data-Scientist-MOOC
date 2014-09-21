91b46b75cc0f6f0039cadea210ab522e76baffb1
strOut- <chrisdaly1988@gmail.com>
2014-09-21 14:59:55 +0100

    19th commit
    cleaned up data products

--- 9 - Developing Data Products/Intro to Shiny/server.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-
-shinyServer(
-  function(input, output) {
-    output$oid1 <- renderPrint({input$id1})
-    output$oid2 <- renderPrint({input$id2})
-    output$odate <- renderPrint({input$date})
-  }
-)
